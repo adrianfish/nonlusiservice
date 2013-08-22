2fb91d2d72a30dadbf6fb34ef39c43584f581f63
Adrian Fish <adrian.r.fish@gmail.com>
2013-08-22 15:54:07 +0100

    sdfsdf

--- src/java/uk/ac/lancs/sakai/nonlusiservice/entityprovider/ModuleProvider.java
+++ src/java/uk/ac/lancs/sakai/nonlusiservice/entityprovider/ModuleProvider.java
@@ -12,12 +12,15 @@ import org.sakaiproject.entitybroker.entityprovider.capabilities.AutoRegisterEnt
 import org.sakaiproject.entitybroker.entityprovider.capabilities.CollectionResolvable;
 import org.sakaiproject.entitybroker.entityprovider.capabilities.Outputable;
 import org.sakaiproject.entitybroker.entityprovider.extension.Formats;
+import org.sakaiproject.entitybroker.entityprovider.search.Restriction;
 import org.sakaiproject.entitybroker.entityprovider.search.Search;
 import org.sakaiproject.entitybroker.util.AbstractEntityProvider;
 import org.sakaiproject.site.api.Site;
 import org.sakaiproject.site.api.SiteService;
 import org.sakaiproject.site.api.SiteService.SelectionType;
 import org.sakaiproject.site.api.SiteService.SortType;
+import org.sakaiproject.user.api.UserDirectoryService;
+import org.sakaiproject.user.api.UserNotDefinedException;
 
 import uk.ac.lancs.sakai.nonlusiservice.Module;
 
@@ -26,6 +29,11 @@ public class ModuleProvider extends AbstractEntityProvider implements CoreEntity
 	private SiteService siteService;
 	public void setSiteService(SiteService siteService) {
 		this.siteService = siteService;
+	}
+	
+	private UserDirectoryService userDirectoryService;
+	public void setUserDirectoryService(UserDirectoryService userDirectoryService) {
+		this.userDirectoryService = userDirectoryService;
 	}
 
 	private DeveloperHelperService developerService = null;
@@ -56,13 +64,36 @@ public class ModuleProvider extends AbstractEntityProvider implements CoreEntity
 	}
 
 	public List<Module> getEntities(EntityReference ref, Search search) {
+		
 		List<Module> modules = new ArrayList<Module>();
 		
 		Map<String,String> criteria = new HashMap<String,String>(1);
-		
 		criteria.put("from-lusi", "no");
 		
-		List<Site> sites = siteService.getSites(SelectionType.ANY, null, null, criteria, SortType.NONE, null);
+		List<Site> sites = new ArrayList<Site>();
+		
+		List<Site> allNonLusiSites = siteService.getSites(SelectionType.ANY, null, null, criteria, SortType.NONE, null);
+		
+		Restriction usernameRes = search.getRestrictionByProperty("username");
+		
+		if(usernameRes != null) {
+			String eid = usernameRes.getStringValue();
+			String userId = null;
+			
+			try {
+				userId = userDirectoryService.getUserByEid(eid).getId();
+			} catch (UserNotDefinedException e) {
+				throw new IllegalArgumentException("'" + eid + "' is not a valid username");
+			}
+			
+			for(Site nonLusiSite : allNonLusiSites) {
+				if(nonLusiSite.getMember(userId) != null) {
+					sites.add(nonLusiSite);
+				}
+			}
+		} else {
+			sites.addAll(allNonLusiSites);
+		}
 		
 		for(Site site : sites) {
 			String siteId = site.getId();
